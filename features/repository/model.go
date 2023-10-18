c98ea8566f68d2ec2f96f95598475bf3a82d5f4a
Trois25 <juanajh428@gmail.com>
2023-10-18 15:44:24 +0700

    fix database

--- features/repository/model.go
+++ features/repository/model.go
@@ -1,12 +1,14 @@
 package repository
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 type Users struct {
-	ID         uuid.UUID `json:"id"`
-	Username   string    `json:"username"`
-	Password   string    `json:"password"`
-	Role_id    uuid.UUID `json:"role_id"`
+	ID            uuid.UUID `json:"id"`
+	Username      string    `json:"username"`
+	Password      string    `json:"password"`
+	Role_id       uuid.UUID `json:"role_id"`
 	Name          string    `json:"name"`
 	Address       string    `json:"address"`
 	Email         string    `json:"email"`
@@ -14,22 +16,17 @@ type Users struct {
 	Phone_number  uuid.UUID `json:"phone_number"`
 	Created_at    string    `json:"created_at"`
 	Updated_at    string    `json:"update_at"`
-	Purchase_id       uuid.UUID `json:"purchase_id"`
+	Purchase_id   uuid.UUID `json:"purchase_id"`
 }
 
-type Purchase struct{
-	ID        uuid.UUID `json:"id"`
-	Ticket_id uuid.UUID `json:"ticket_id"`
-	User_id uuid.UUID `json:"user_id"`
-	Quantity  int       `json:"quantity"`
-	Total_price float64 `json:"total_price"`
-}
-
-type Ticket struct {
-	ID        uuid.UUID `json:"id"`
-	Quantity  int       `json:"quantity"`
-	Price float64 `json:"price"`
-	Events_id uuid.UUID `json:"Events_id"`
+type Purchase struct {
+	ID             uuid.UUID `json:"id"`
+	Ticket_id      uuid.UUID `json:"ticket_id"`
+	User_id        uuid.UUID `json:"user_id"`
+	Quantity       int       `json:"quantity"`
+	Total_price    float64   `json:"total_price"`
+	Booking_code   string    `json:"booking_code"`
+	Payment_status string    `json:"payment_status"`
 }
 
 type Roles struct {
@@ -39,9 +36,12 @@ type Roles struct {
 }
 
 type Events struct {
-	ID         uuid.UUID `json:"id"`
-	Title      string    `json:"title"`
-	Body       string    `json:"body"`
-	Status     string    `json:"status"`
-	Created_at string    `json:"created_at"`
+	ID              uuid.UUID `json:"id"`
+	Title           string    `json:"title"`
+	Body            string    `json:"body"`
+	Ticket_quantity int       `json:"ticket_quantity"`
+	Price           float64   `json:"price"`
+	Status          string    `json:"status"`
+	Date            string    `json:"date"`
+	Created_at      string    `json:"created_at"`
 }
--- features/ticket/repository/model.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "github.com/google/uuid"
-
-type Ticket struct{
-	ID uuid.UUID `json:"id"`
-	Quantity    int `json:"quantity"`
-	Events_id uuid.UUID `json:"Events_id"`
-}
